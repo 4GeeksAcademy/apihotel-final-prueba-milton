04d394af81e2ff40e569dc26b00560c371ed0295
Milton Orlando Diaz Alvarez <162182668+allisonjuliana@users.noreply.github.com>
2025-04-04 00:15:54 +0000

    render deploy 4

--- src/wsgi.py
+++ src/wsgi.py
@@ -5,8 +5,10 @@ import os
 import sys
 
 # Agregar el directorio actual al path para que Python pueda encontrar los módulos
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
 
+# Importar la aplicación
 from app import app as application
 
 if __name__ == "__main__":
